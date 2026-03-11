7e8f9cc7666e76c29da1a23f752e3535dae4c6cb
Joya <fsbusinesssolutions01@gmail.com>
2026-03-11 15:42:29 +0530

    fix bug update on 11-3

--- app/products/[slug]/ProductDescription.tsx
+++ app/products/[slug]/ProductDescription.tsx
@@ -2,6 +2,7 @@
 
 import DOMPurify from 'dompurify';
 import parse from 'html-react-parser';
+import { sanitizeHTML } from '@/lib/utils/sanitize';
 
 export default function ProductDescription({ html }: { html: string }) {
   const clean = DOMPurify.sanitize(html);
