ddb4518e695c544a9195a7116987bd3974a73200
Joya <fsbusinesssolutions01@gmail.com>
2026-03-26 13:16:04 +0530

    Update 26.08

--- lib/algolia.ts
+++ lib/algolia.ts
@@ -1,7 +1,9 @@
-// lib/algoliaClient.ts
 import { algoliasearch } from "algoliasearch";
 
-export const algoliaClient = algoliasearch(
-  process.env.NEXT_PUBLIC_ALGOLIA_APP_ID!,
-  process.env.NEXT_PUBLIC_ALGOLIA_SEARCH_KEY!
-);
+const appId = process.env.NEXT_PUBLIC_ALGOLIA_APP_ID;
+const apiKey = process.env.NEXT_PUBLIC_ALGOLIA_SEARCH_KEY;
+
+export const algoliaClient =
+  appId && apiKey
+    ? algoliasearch(appId, apiKey)
+    : null;
