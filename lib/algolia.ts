24791b7b3eedbd8e0960d9fa1c5ef4c008faed3f
Joya <fsbusinesssolutions01@gmail.com>
2026-03-27 07:27:28 +0530

    algolia fix

--- lib/algolia.ts
+++ lib/algolia.ts
@@ -3,7 +3,4 @@ import { algoliasearch } from "algoliasearch";
 const appId = process.env.NEXT_PUBLIC_ALGOLIA_APP_ID;
 const apiKey = process.env.NEXT_PUBLIC_ALGOLIA_SEARCH_KEY;
 
-export const searchClient =
-  appId && apiKey
-    ? algoliasearch(appId, apiKey)
-    : null;
+export const searchClient = algoliasearch(appId, apiKey);
