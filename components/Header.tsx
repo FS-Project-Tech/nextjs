9a7242978457878b0e088272c242172d2974b4a2
Joya <fsbusinesssolutions01@gmail.com>
2026-03-27 08:48:55 +0530

    algolia fix v5

--- components/Header.tsx
+++ components/Header.tsx
@@ -126,7 +126,7 @@ export default function Header() {
 				</div>
 
 				{/* Center Search */}
-				<div className="hidden md:flex lg:col-span-7 justify-center">
+				<div className="flex w-full justify-center">
 					
 				<SearchBar />
 					
